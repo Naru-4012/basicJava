d07e6ee588a43db8cbd862d2e4e95e282f8f119c
Naru-4012 <narendrakorade173@gmail.com>
2023-03-18 11:24:02 +0530

    First Commit

---
+++ Person.java
@@ -0,0 +1,17 @@
+package com.basic.demo;
+
+public class Person {
+
+	public static void main(String[] args) {
+		
+		int adhaar;
+		String name;
+		static String country = "India";
+		
+		
+		public Person() //no-arg constructor
+		{
+			super();
+		}
+
+}
---
+++ WrapperDemo.java
@@ -0,0 +1,13 @@
+package com.basic.demo;
+
+public class WrapperDemo {
+
+	public static void main(String[] args) {
+
+		Integer i = 10;
+		
+		System.out.println(Integer.toHexString(i));
+
+	}
+
+}
